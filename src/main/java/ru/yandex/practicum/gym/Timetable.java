12b042242588a16968c09fd1d61cdb8bfb9bda9f
Lera Tikhonova <valeria.tihonova2011@yandex.ru>
2026-05-15 14:48:23 +0300

    Fix Timetable.java: add proper implementation and fix syntax error

--- src/main/java/ru/yandex/practicum/gym/Timetable.java
+++ src/main/java/ru/yandex/practicum/gym/Timetable.java
@@ -3,18 +3,43 @@ package ru.yandex.practicum.gym;
 import java.util.*;
 
 public class Timetable {
-
-    private /* как это хранить??? */ timetable;
-
+    
+    private Map<DayOfWeek, Map<TimeOfDay, List<TrainingSession>>> schedule;
+    
+    public Timetable() {
+        schedule = new HashMap<>();
+        // Инициализируем структуру для всех дней недели
+        for (DayOfWeek day : DayOfWeek.values()) {
+            schedule.put(day, new HashMap<>());
+            for (TimeOfDay time : TimeOfDay.values()) {
+                schedule.get(day).put(time, new ArrayList<>());
+            }
+        }
+    }
+    
     public void addNewTrainingSession(TrainingSession trainingSession) {
-        //сохраняем занятие в расписании
+        DayOfWeek day = trainingSession.getDayOfWeek();
+        TimeOfDay time = trainingSession.getTimeOfDay();
+        
+        if (schedule.containsKey(day) && schedule.get(day).containsKey(time)) {
+            schedule.get(day).get(time).add(trainingSession);
+        }
     }
-
-    public /* непонятно, что возвращать */ getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    
+    public List<TrainingSession> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+        List<TrainingSession> result = new ArrayList<>();
+        if (schedule.containsKey(dayOfWeek)) {
+            for (TimeOfDay time : TimeOfDay.values()) {
+                result.addAll(schedule.get(dayOfWeek).get(time));
+            }
+        }
+        return result;
     }
-
-    public /* непонятно, что возвращать */ getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
+    
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+        if (schedule.containsKey(dayOfWeek) && schedule.get(dayOfWeek).containsKey(timeOfDay)) {
+            return new ArrayList<>(schedule.get(dayOfWeek).get(timeOfDay));
+        }
+        return new ArrayList<>();
     }
 }
