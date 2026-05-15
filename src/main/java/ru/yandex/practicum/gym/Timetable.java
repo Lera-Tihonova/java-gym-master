ef05013b9fb6e014f73c4b0ee48496f2ce20ec57
Lera Tikhonova <valeria.tihonova2011@yandex.ru>
2026-05-15 14:51:44 +0300

    Fix trailing whitespace in Timetable.java

--- src/main/java/ru/yandex/practicum/gym/Timetable.java
+++ src/main/java/ru/yandex/practicum/gym/Timetable.java
@@ -3,9 +3,9 @@ package ru.yandex.practicum.gym;
 import java.util.*;
 
 public class Timetable {
-    
+
     private Map<DayOfWeek, Map<TimeOfDay, List<TrainingSession>>> schedule;
-    
+
     public Timetable() {
         schedule = new HashMap<>();
         // Инициализируем структуру для всех дней недели
@@ -16,16 +16,16 @@ public class Timetable {
             }
         }
     }
-    
+
     public void addNewTrainingSession(TrainingSession trainingSession) {
         DayOfWeek day = trainingSession.getDayOfWeek();
         TimeOfDay time = trainingSession.getTimeOfDay();
-        
+
         if (schedule.containsKey(day) && schedule.get(day).containsKey(time)) {
             schedule.get(day).get(time).add(trainingSession);
         }
     }
-    
+
     public List<TrainingSession> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
         List<TrainingSession> result = new ArrayList<>();
         if (schedule.containsKey(dayOfWeek)) {
@@ -35,7 +35,7 @@ public class Timetable {
         }
         return result;
     }
-    
+
     public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
         if (schedule.containsKey(dayOfWeek) && schedule.get(dayOfWeek).containsKey(timeOfDay)) {
             return new ArrayList<>(schedule.get(dayOfWeek).get(timeOfDay));
